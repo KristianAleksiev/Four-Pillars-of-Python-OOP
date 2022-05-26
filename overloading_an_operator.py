371dd3fc41dee8dd45d72cc7e78615f30a2fffed
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-26 20:30:46 +0300

    Add files via upload

---
+++ overloading_an_operator.py
@@ -0,0 +1,16 @@
+# Overloading an operator
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def __add__(square_one, square_two):
+        return(4 * square_one.side) + (4 * square_two.side)
+
+
+square_one = Square(5)
+square_two = Square(10)
+#Sum of sides of squares
+
+result = square_one + square_two
+print(result)
